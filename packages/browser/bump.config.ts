3bbfa314842e210a3db24e15c85a1d054e9ebeb9
terminaate <terminaatecorp@gmail.com>
2026-03-01 22:24:31 +0300

    add bumpp to project

---
+++ packages/browser/bump.config.ts
@@ -0,0 +1,7 @@
+import { defineConfig } from 'bumpp';
+
+export default defineConfig({
+  tag: false,
+  commit: 'chore: bump browser package version to v%s',
+  push: false,
+});
---
+++ packages/client/bump.config.ts
@@ -0,0 +1,7 @@
+import { defineConfig } from 'bumpp';
+
+export default defineConfig({
+  tag: false,
+  commit: 'chore: bump client package version to v%s',
+  push: false,
+});
