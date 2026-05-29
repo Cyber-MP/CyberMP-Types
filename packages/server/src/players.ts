f006b3ebcdb84e8a84cfb2a8c099c0042322ab8e
terminaate <terminaatecorp@gmail.com>
2026-05-29 21:26:09 +0300

    docs(server): add category to player entity

--- packages/server/src/players.ts
+++ packages/server/src/players.ts
@@ -2,6 +2,9 @@ import type { EntityType, MpEntities, MpEntity } from './entities';
 import type { Vector3 } from './shared';
 import type { MpVehicle } from './vehicles';
 
+/**
+ * @category Entities/Players
+ */
 export interface MpPlayer extends MpEntity<EntityType.Player> {
   readonly velocity: Vector3;
   readonly health: number;
@@ -17,6 +20,9 @@ export interface MpPlayer extends MpEntity<EntityType.Player> {
   mute(): void;
 }
 
+/**
+ * @category Entities/Players
+ */
 export interface MpPlayers extends MpEntities<MpPlayer> {
   kick(player: MpPlayer | number, reason?: string): void;
   toArray(): MpPlayer[];
