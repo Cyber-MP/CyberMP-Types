4a278540acaaf1eb7a3040cbba092261949ef6c2
terminaate <terminaatecorp@gmail.com>
2026-06-06 13:02:49 +0300

    feat: add CI workflows for package releases

--- packages/browser/bump.config.ts
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'bumpp';
-
-export default defineConfig({
-  tag: false,
-  commit: 'chore: bump browser package version to v%s',
-  push: false,
-});
--- packages/client/bump.config.ts
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'bumpp';
-
-export default defineConfig({
-  tag: false,
-  commit: 'chore: bump client package version to v%s',
-  push: false,
-});
