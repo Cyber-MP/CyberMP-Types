8197b9d7a70fa83a348c70a9ad70d0af67280948
terminaate <terminaatecorp@gmail.com>
2026-06-06 17:41:34 +0300

    chore: add bumpp configuration to packages

---
+++ packages/browser/bump.config.ts
@@ -0,0 +1,5 @@
+import { defineConfig } from 'bumpp';
+
+export default defineConfig({
+  commit: 'chore(browser): release v%s',
+});
---
+++ packages/client/bump.config.ts
@@ -0,0 +1,5 @@
+import { defineConfig } from 'bumpp';
+
+export default defineConfig({
+  commit: 'chore(client): release v%s',
+});
